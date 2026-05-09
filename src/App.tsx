2c614d8bba9060b10ecaa56f9fe3484080c9dcbd
Collins Michael <144531933+Aeyjjjk@users.noreply.github.com>
2026-05-09 09:49:06 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/ThemeProvider";
-import { ScrollToTop } from "@/components/ScrollToTop";
+import { ScrollToTop } from "@/components/ScrollToTop"; 
 import { LoadingSpinner } from "@/components/LoadingSpinner";
 import Index from "./pages/Index";
 import ProjectDetail from "./pages/ProjectDetail";
